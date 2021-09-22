42f263809c6e728d4ebd126ac5915163b980402b
culi <culitif@tuta.io>
2021-09-22 03:06:55 -0700

    rework styling

--- src/components/app/Header.jsx
+++ src/components/app/Header.jsx
@@ -5,9 +5,16 @@ const Header = () => {
 
   const location = useLocation();
 
+  const page = location.pathname;
+
   return <>
-    {location.pathname !== '/' && <header className="Header">
+    {page !== '/' && <header className="Header">
       <Link to="/">work.culi</Link>
+      <nav>
+        <Link to="/">home</Link>
+        {page !== '/now' && <Link to="/now">now</Link>}
+        {page !== '/uses' && <Link to="/uses">uses</Link>}
+      </nav>
     </header>}
   </>;
 };
